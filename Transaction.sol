ef1281b83ffda8352add9e637b6da9f32f1cbc57
zeredogo <zeredogo@gmail.com>
2022-12-19 05:49:25 +1400

    update commit

--- Transaction.sol
+++ Transaction.sol
@@ -5,7 +5,7 @@ contract Transaction {
     uint256 Total;
     uint256 Grade;
     string Destination;
-    strikng
+    string d
 
     function data (string memory _myProfile) {
         return _myProfile
