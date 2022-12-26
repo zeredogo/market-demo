dc36b717eef00c129d81e74f24271f1aec8c72dc
zeredogo <zeredogo@gmail.com>
2022-12-26 15:01:37 +1400

    changes made

--- Transaction.jsx
+++ Transaction.jsx
@@ -7,7 +7,7 @@ const Transaction = () => {
         <h2> May the Lord guild me on this path</h2>
         <p>I started this with a good faith,
           i need to bring the good faith back and 
-          work harder than i use to
+          work harder than i use to, Lord please i need you.
         </p>
     </div>
   )
