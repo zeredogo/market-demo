d96a7fc2e97498776aa3614bf33928b4eafff98a
zeredogo <zeredogo@gmail.com>
2023-01-02 02:32:01 +1400

    changes made

--- Body.jsx
+++ Body.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div>
+        <h1>This is the start of a new begining.</h1>
+        <h2> May the Lord guild me on this path</h2>
+        <p>I started this with a good faith,
+          i need to bring the good faith back and 
+          work harder than i use to, Lord please i need you.
+        </p>
+    </div>
   )
 }
 
--- Transaction.jsx
+++ Transaction.jsx
@@ -6,12 +6,7 @@ const Transaction = ({name}) => {
   return (
     <div>
         <App />
-        <h1>This is the start of a new begining.</h1>
-        <h2> May the Lord guild me on this path</h2>
-        <p>I started this with a good faith,
-          i need to bring the good faith back and 
-          work harder than i use to, Lord please i need you.
-        </p>
+        
         <Button name={name}/>
     </div>
   )
