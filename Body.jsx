4cc5cc7be49e71e76f98736f1d8f113d7f1174c8
zeredogo <zeredogo@gmail.com>
2023-01-03 08:29:47 +1400

    changes made

--- Body.jsx
+++ Body.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Body = () => {
   return (
     <div>
-        <h1>This is the start of a new begining.</h1>
+        <h1 className='title' >This is the start of a new begining.</h1>
         <h2> May the Lord guild me on this path</h2>
         <p>I started this with a good faith,
           i need to bring the good faith back and 
