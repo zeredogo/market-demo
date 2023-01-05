c5a9602d1d7133fb6f280add751b8f01a836a9f5
zeredogo <zeredogo@gmail.com>
2023-01-06 02:07:48 +1400

    changes made

--- Body.jsx
+++ Body.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Body = () => {
   return (
     <div>
-        <h1 className='title' >This is the start of a new begining.</h1>
+        <h1 className='title' >This is the start with hook of a new begining.</h1>
         <h2> May the Lord guild me on this path</h2>
         <p className='note' >I started this has for this with a good faith,
           i need to bring the good faith  back and tie 08128548824 
