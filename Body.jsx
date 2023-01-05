89a44b5276eb22e598750b54f6b1b982a8c47205
zeredogo <zeredogo@gmail.com>
2023-01-05 13:03:50 +0100

    changes made

--- Body.jsx
+++ Body.jsx
@@ -5,7 +5,7 @@ const Body = () => {
     <div>
         <h1 className='title' >This is the start of a new begining.</h1>
         <h2> May the Lord guild me on this path</h2>
-        <p className='note' >I started for this with a good faith,
+        <p className='note' >I started this for this with a good faith,
           i need to bring the good faith back and 08128548824 
           work harder than i use to, Lord please i need you.
         </p>
