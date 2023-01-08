fd9a551e8d3e7acf1e677087787261b537598d1f
zeredogo <zeredogo@gmail.com>
2023-01-07 14:06:18 -1200

    changes made

--- Button.jsx
+++ Button.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Button = ({name}) => {
   return (
+    
     <div>
       <h1>{name}</h1>
-      <p className='note' >this is all THERE is hope the list of name around the world </p>
+      <p className='note' >this is all THERE is hope never the list of name around the world </p>
       <div>
         <p className='btn'><Button>Click</Button></p>
         
