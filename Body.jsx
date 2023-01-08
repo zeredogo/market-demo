ce1478c5551a6548535ab218fc20acde037dbe75
zeredogo <zeredogo@gmail.com>
2023-01-07 14:09:18 -1200

    changes made

--- Body.jsx
+++ Body.jsx
@@ -6,7 +6,7 @@ const Body = () => {
         <h1 className='title' >This is the start with hook of a new begining.</h1>
         <h2> May the Lord guild me universal on this path gate </h2>
         <p className='note' >I started this has for return this with a good faith,
-          i need to bring the good faith what can  back and  they monday tie 08128548824 
+          i need to bring the good faith what can love do back and  they monday tie 08128548824 
           work harder than twitter i use to,  Lord please i need you.
         </p>
     </div>
