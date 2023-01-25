3c6365c0da98cf4213525cf5899726d829d6baf4
zeredogo <zeredogo@gmail.com>
2023-01-26 08:04:53 +1400

    update made

--- Body.jsx
+++ Body.jsx
@@ -7,7 +7,7 @@ const Body = () => {
         <h2> May the Lord guild me universal on this path gate </h2>
         <p className='note' >I started this has for return this with a good faith,
           i need to bring the good faith, what can love do back and  they monday tie 08128548824 
-          work harder than twitter i use to, what i i saying here  nice to Lord please i need you.
+          work harder than twitter i use to, what i i saying here  nice to know Lord please i need you.
         </p>
     </div>
   )
