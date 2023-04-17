4966c16254cba9ae870b39d54ee0ea106127d22d
zeredogo <zeredogo@gmail.com>
2023-04-18 13:39:52 +1400

    update made

--- MyContract.sol
+++ MyContract.sol
@@ -4,4 +4,8 @@ contract MyContract {
     function setName(string memory _name) public {
         name = _name;
     }
+
+    function getName() public view returns(string memory) {
+        return name;
+    }
 }
