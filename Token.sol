9ff9e719b6066cb923d6765d17cb33cd4e009493
zeredogo <zeredogo@gmail.com>
2023-04-19 12:47:52 +1400

    update made

--- Token.sol
+++ Token.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.0;
 contract Token is Ownable, MinterRole, ERC721Full {
     using SafeMath for uint;
 
-    constructor() ERC721Full("Token", "TKN") public {
+    constructor() ERC721Full("Token", "TKN") public {   
+    }
+
+    function mint(address _to, string memory _tokenURI) public onlyMinter returns(bool) {
         
     }
 }
