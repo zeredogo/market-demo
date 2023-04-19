927c503d5a0c7297aa74a46bc4199e758bde497d
zeredogo <zeredogo@gmail.com>
2023-04-20 04:32:50 +1400

    update made

--- MyContract.sol
+++ MyContract.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 contract MyContract {
     string name = "Name 1";
     string private name1 = "Name 2";
+    string internal name2 = "Name 3";
+    string public name3 = "Name 4";
 
     function setName(string memory _name) public {
         name = _name;
