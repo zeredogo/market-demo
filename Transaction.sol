4dbf4928ea5f10423165cf0682c53dee18f85087
zeredogo <zeredogo@gmail.com>
2023-04-22 23:47:27 +1400

    update made

--- Transaction.sol
+++ Transaction.sol
@@ -38,6 +38,8 @@ contract Transaction {
     function evenOrOdd1(uint x) public pure returns (string memory) {
         if (x % 2 == 0) {
             return 'even';
+        } else {
+            return 'odd';
         }
     }
 }
