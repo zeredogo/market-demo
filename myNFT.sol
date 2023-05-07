12f57adf2e28f1f19b1860bb5ad8931386f4b6e2
zeredogo <zeredogo@gmail.com>
2023-05-07 14:15:46 +1400

    update made

--- myNFT.sol
+++ myNFT.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract myNFT is ERC721, Ownable
+contract myNFT is ERC721, Ownable {
+    uint256 public constant MAX_SUPPLY = 10;
+}
