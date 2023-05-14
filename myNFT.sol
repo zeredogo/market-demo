c504c0bbfa6c2d89ba1fb87580d05929f4442f15
zeredogo <zeredogo@gmail.com>
2023-05-13 14:40:47 -1200

    update made

--- myNFT.sol
+++ myNFT.sol
@@ -6,6 +6,7 @@ contract myNFT is ERC721, Ownable {
     uint256 public constant MAX_SUPPLY = 10;
     uint256 public constant PRICE = 0.01 ether;
     string public NAME = "Paul";
+    address  
     
 
     function setName(string memory _name) public {
