54fea3a97dff25afb0c6e6e680b0bf05708975b2
zeredogo <zeredogo@gmail.com>
2023-05-14 16:49:48 +1400

    update made

--- myNFT.sol
+++ myNFT.sol
@@ -18,5 +18,5 @@ contract myNFT is ERC721, Ownable {
     function resetName() public{
         delete NAME;
     }
-    function writeAddress(uint256 _address)
+    function writeAddress(uint256 _address) public {}
 }
