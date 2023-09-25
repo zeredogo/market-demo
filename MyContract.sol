3428d464b5347b0e3bd8d709656ed02b9c376dca
zeredogo <zeredogo@gmail.com>
2023-09-25 13:49:51 +0100

    update commit

--- MyContract.sol
+++ MyContract.sol
@@ -7,7 +7,7 @@ contract MyContract {
     string internal name2 = "Name 3";
     string public name3 = "Name 4";
     string public names;
-    uint public pr
+    uint public pri
 
     mapping(uint => string) public myMapping;
 
