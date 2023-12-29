4b873f410ce121791e35bb1e90e90d1ce29ca503
zeredogo <zeredogo@gmail.com>
2023-12-29 23:13:36 +0100

    update commit

--- myNFT.sol
+++ myNFT.sol
@@ -14,7 +14,7 @@ contract myNFT is ERC721, Ownable {
     function setName(string memory _name) public {
         NAME = _name;
     }
-    function getName() public view r{
+    function getName() public view re{
         return NAME;
     }
     function resetName() public{
