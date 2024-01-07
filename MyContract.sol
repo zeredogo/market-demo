d326141b65fac853da0a1c19636f18784874ba60
zeredogo <zeredogo@gmail.com>
2024-01-07 23:35:49 +0100

    update commit

--- MyContract.sol
+++ MyContract.sol
@@ -12,7 +12,7 @@ contract MyContract {
     mapping(uint => string) public myMapping;
 
     function setName(string memory _name) public 
-        ret
+        retu
     }
 
     function setPrice(uint _price) external {
